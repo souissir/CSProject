5be00f832edaa3ae93ba2350d6c1d7f95cf63ad9
rania.souissi <rania.souissi@ensiie.com>
2024-11-19 18:04:16 +0100

    Définition de l'interface de solver

---
+++ hd/solver.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <armadillo>
+
+#ifndef SOLVER_H
+#define SOLVER_H
+
+
+class Solver {
+     private : 
+        int S;
+        int n;
+
+    
+         
+
+
+};
+
+#endif //SOLVER_H
